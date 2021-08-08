f59acb7f86d1c4965f6b9aa9f0015a44e682d926
Nikolay Rusanov <nikolarusano92@gmail.com>
2021-08-08 14:20:06 +0300

    Finish_Tasks

--- src/Task1.java
+++ src/Task1.java
@@ -8,7 +8,7 @@ public class Task1 {
 
         Scanner scanner= new Scanner(System.in);
         do{
-            System.out.println("Input number of task:" +
+            System.out.println("Input number of task:\n" +
                     "1 - inch into cm and cm into inches" + "\n" +
                     "2 - Vasya's Euro course money" + "\n" +
                     "3 - 2 signs number partially" + "\n" +
@@ -29,7 +29,7 @@ public class Task1 {
                 double vasyaSalary = 5000.0, parentGift = 5000.0, summaryMoneyHrn;
                 summaryMoneyHrn = vasyaSalary + parentGift;
                System.out.println("According to the initial data Vasya has for travelling abroad" + "\n" +
-                       "sum in hrn: " + summaryMoneyHrn + "and " + "\n" +
+                       "sum in hrn: " + summaryMoneyHrn + " and " + "\n" +
                        "sum in Eur: " + grnToEur(summaryMoneyHrn));
 
            }else if(choice == 3){
@@ -39,11 +39,10 @@ public class Task1 {
                x = scanner.nextInt();
                if (x<100 & x>9) {
                    getStartAndFinishNum(x, left, right);
-                   System.out.println("The result execution of number " + x + "" +
-                           "is: " + right + "  " + left);
+
 
                }else{
-                   System.out.printf("The inputted number is not 2 digit");
+                   System.out.printf("The inputted number is not 2 digit\n");
                }
            }else if(choice ==4){
                int x ;
@@ -86,6 +85,30 @@ public class Task1 {
                    System.out.printf("The inputted number is not 2 digit");
                }
 
+           }else if(choice ==7){
+               int x;
+               boolean isPositive = false;
+               System.out.printf("Input x number please:");
+               x = scanner.nextInt();
+               getNumberSign(x,isPositive);
+
+           }else if(choice == 8){
+               int x;
+               System.out.printf("Input x number please:");
+               x = scanner.nextInt();
+               getNumberQuantityDigitsAndSign(x);
+
+           }else if(choice == 9){
+               int x;
+               System.out.printf("Input x number please:");
+               x = scanner.nextInt();
+               if (x<100 && x>9 && x > 0) {
+
+                   getBiggerFromNumbersSumAndNumberValue(x);
+
+               }else{
+                   System.out.printf("The inputted number is not 2 digit or less than 0");
+               }
            }
 
         }while (choice!=0);
@@ -103,9 +126,13 @@ public class Task1 {
     }
     public static void getStartAndFinishNum (int x, char left, char right){
         String temp = Integer.toString(x);
+
         char [] tempCarArr = temp.toCharArray();
         left = tempCarArr[0];
+
         right = tempCarArr[1];
+        System.out.println("The result execution of number " + x + "" +
+                "is: " + right + "  " + left);
     }
     public static void getFullViewNum (int x, int left, int right){
         right = x%10;
@@ -113,6 +140,8 @@ public class Task1 {
         int temp = x/10;
 
         left = temp*10;
+        System.out.println("The weight of number " + x + "" +
+                "is: " + left + "+" + right);
     }
     public static int getWeight (int x, int left, int right){
         right = x%10;
@@ -131,6 +160,41 @@ public class Task1 {
             parityDegree++;
         }
         return parityDegree;
+
+    }
+    public static void getNumberSign(int x, boolean isPositive){
+        if(x < 0){
+            System.out.printf("The number is positive: " + isPositive);
+        }else if(x>0){
+            isPositive = true;
+            System.out.println("The number is positive: " + isPositive + "\n");
+
+        }else if(x == 0){
+            System.out.println("The number x is " + x + " don't have sign" + "\n");
+        }else {
+            System.out.println("Incorrect input" + "\n");
+        }
+    }
+    public  static void getNumberQuantityDigitsAndSign(int x){
+        if(x>0 && x>99 && x<1000){
+            System.out.println("The number " + x +" is positive and 3 digit that's why :\n" + (x-1));
+        }else{
+            System.out.println("You input wrong number");
+        }
+    }
+    public static void getBiggerFromNumbersSumAndNumberValue(int x){
+        int right = x%10;
+
+        int left = x/10;
+        int production = left*right;
+
+        if(production<x){
+            System.out.println("The production of numbers x "+ production + " is less than value " + x);
+        }else if(production>x){
+            System.out.println("The production of numbers x "+ production + " is bigger than value " + x);
+        }else if(production==x){
+            System.out.println("The production of numbers x "+ production + " is equal than value " + x);
+        }
 
     }
 
---
+++ src/TaskSheepAndWolfs.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TaskSheepAndWolfs {
+    public static void main(String[] args) {
+        System.out.println("Input coordinates X and Y (from 0 till 12) \n input X:");
+        Scanner scanner = new Scanner(System.in);
+        int X, Y;
+
+            X = scanner.nextInt();
+            System.out.println("input Y: ");
+            Y = scanner.nextInt();
+
+        if(X < 2 && X > 10  && Y < 2 && Y > 9){
+            System.out.println("Coordinates You're inputted out of the range");
+        }else if(X>=2 && X<=4 && Y >=2 && Y <= 9 || X >= 8 && X <=10 && Y >=2 && Y <= 9 || Y >= 2 && Y <= 4 && X >= 4 && X <=8 || Y >= 7 && Y <= 7 && X >= 4 && X <=8 ){
+            System.out.println("Coordinates You're inputted - area of the wolfs");
+        }else if(X>=4 && X<=8 && Y >=3 && Y <= 7 ){
+            System.out.println("Coordinates You're inputted - area of the sheep");
+        }else {
+            System.out.println("Out of the range input");
+    }
+    }
+}
