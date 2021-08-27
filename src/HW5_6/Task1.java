31384adda0aed110bf4517e01efdfa437c416a8c
Nikolay Rusanov <nikolarusano92@gmail.com>
2021-08-28 00:38:42 +0300

    HW5_6_task1

---
+++ src/HW5_6/Task1.java
@@ -0,0 +1,61 @@
+package HW5_6;
+
+public class Task1 {
+
+    public static void LastCharSymbol(String str){
+        char lastChar = str.charAt(str.length()-1);
+        System.out.println("The last char at string is: " + lastChar);
+    }
+
+    public static boolean isEndsWithSubStr(String str){
+        if(str.endsWith("!!!")){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isStartsWithSubStr(String str){
+        if(str.startsWith("I like")){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isContainSubStr(String str){
+        if(str.contains("Java")){
+            return true;
+        }
+        return false;
+    }
+
+    public static void findPositionSubString(String str){
+        if(str.contains("Java")){
+            System.out.println("The position of substring Java in String is: " + str.indexOf("Java"));
+        }
+        System.out.println("The string doesn't contain substring Java");
+    }
+
+    public static void replaceALetterToO(String str){
+        System.out.println("The String after replacement A to O is:" + str.replace("a","o"));
+    }
+
+    public static void replaceToUpperCase(String str){
+        System.out.println("The String after update into UpperCase is:" + str.toUpperCase());
+    }
+
+    public static void replaceToLowerCase(String str){
+        System.out.println("The String after update into LowerCase is:" + str.toLowerCase());
+    }
+
+    public static void cutOutSubString(String str){
+        String substr = str.substring(str.indexOf("Java"),str.indexOf("Java")+4);
+    }
+
+
+
+    public static void main(String[] args) {
+        String str = "I like C# more than Java!!!";
+
+
+    }
+}
