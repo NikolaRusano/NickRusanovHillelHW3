571c861b081eab666bc450e5bdbcb1a42037de7c
Nikolay Rusanov <nikolarusano92@gmail.com>
2021-08-30 21:05:27 +0300

    Commit_task2_HW7

--- src/HW7/Task2HW_Strings.java
+++ src/HW7/Task2HW_Strings.java
@@ -2,8 +2,22 @@ package HW7;
 
 public class Task2HW_Strings {
 
+    //выясните, под каким номером встроке стоит первая буква я.
+    public static void getIndexFirstYaSymbol(String str){
+        char [] arr = str.toCharArray();
+        String ya = "я";
+        for (int i = 0; i < arr.length; i++){
+
+            if (arr[i] == ya){
+                System.out.println("The ");
+            }
+        }
+    }
 
     public static void main(String[] args) {
+        String str = "Приключения вчерашнего найденыша, а ныне мага-демонолога " +
+                "Неда Черного продолжаются!Что может быть красивее летящего в небе дракона?" +
+                " Но не нужно забывать – это не игрушка, это довольно умное и опасное";
 
     }
 
