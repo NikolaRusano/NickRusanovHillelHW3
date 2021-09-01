d2ab344f38e33ae0658bfbdc9900f5645081cef0
Nikolay Rusanov <nikolarusano92@gmail.com>
2021-09-01 10:22:43 +0300

    The_HW7_Task2_Finished

--- src/HW7/Task2HW_Strings.java
+++ src/HW7/Task2HW_Strings.java
@@ -5,11 +5,24 @@ public class Task2HW_Strings {
     //выясните, под каким номером встроке стоит первая буква я.
     public static void getIndexFirstYaSymbol(String str){
         char [] arr = str.toCharArray();
-        String ya = "я";
+        char ya = 'я';
         for (int i = 0; i < arr.length; i++){
+            //String temp = arr[i].toString();
+            if ( ya == arr[i]){
+                System.out.println("The first Я symbol is locate under " + i + " char array index");
+                break;
+            }
+        }
+    }
 
-            if (arr[i] == ya){
-                System.out.println("The ");
+    public static void getIndexLastYaSymbol(String str){
+        char [] arr = str.toCharArray();
+        char ya = 'я';
+        for (int i = arr.length-1; i > 0; i--){
+            //String temp = arr[i].toString();
+            if ( ya == arr[i]){
+                System.out.println("The Last Я symbol is locate under " + i + " char array index");
+                break;
             }
         }
     }
@@ -18,6 +31,9 @@ public class Task2HW_Strings {
         String str = "Приключения вчерашнего найденыша, а ныне мага-демонолога " +
                 "Неда Черного продолжаются!Что может быть красивее летящего в небе дракона?" +
                 " Но не нужно забывать – это не игрушка, это довольно умное и опасное";
+
+        getIndexFirstYaSymbol(str);
+        getIndexLastYaSymbol(str);
 
     }
 
