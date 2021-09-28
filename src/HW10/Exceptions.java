aea51bba79e09cc4dadce5012f07669250f12c3b
NikolaRusano <36786249+NikolaRusano@users.noreply.github.com>
2021-09-28 23:11:26 +0300

    HW10_Exceptions_update3

--- src/HW10/Exceptions.java
+++ src/HW10/Exceptions.java
@@ -22,6 +22,7 @@ import java.util.Scanner;
 
 public class Exceptions {
 
+
     public static void main(String[] args) throws WrongLoginException, WrongPasswordException{
         boolean isLoginContainUnderscore=false, isLoginContainMoreThan20Char=false, isPasswordContainExclamationPoint=false,
                 isPasswordContainMoreThan20Char=false,isPasswordSameToConfPassw=false;
@@ -29,37 +30,66 @@ public class Exceptions {
 
         System.out.println("Input Login:");
         Scanner scanner = new Scanner(System.in);
+        String s = "The Login doesn't contain _ sign";
         try {
             String login = scanner.nextLine();
             System.out.println("Input Password:");
             String password = scanner.nextLine();
             System.out.println("Input confirmation Password:");
-            String confirPassword = scanner.nextLine();
-            isLoginContainUnderscore = login.contains("_");
-            isLoginContainMoreThan20Char = login.length() >= 20;
-            isPasswordContainExclamationPoint = password.contains("!");
-            isPasswordContainMoreThan20Char = password.length()>=20;
-            isPasswordSameToConfPassw = (password == confirPassword);
-            if (isLoginContainUnderscore) {
-                throw new WrongLoginException("The Login doesn't contain _ sign");
-            } else if (isLoginContainMoreThan20Char) {
-                throw new WrongLoginException("The Login doesn't contain 20 or more than 20 characters");
-            }else if(isPasswordContainExclamationPoint){
-                throw new WrongPasswordException("The Password doesn't contain ! sign");
-            }else if(isPasswordContainMoreThan20Char){
-                throw new WrongPasswordException("The Password doesn't contain 20 or more than 20 characters");
-            }else  if(isPasswordSameToConfPassw){
-                throw new WrongPasswordException("The Password doesn't corresponds to confirmPassword");
-            }
-                System.out.println("The login contain correct input format");
+            String confirmPassword = scanner.nextLine();
+            isLoginContainUnderscore(login);
+            isLoginContainMoreThan20Char(login);
+
+            isPasswordContainExclamationPoint(password);
+            isPasswordContainMoreThan20Char(password);
+            isPasswordSameToConfPassw(password,confirmPassword);
+
+        }catch (WrongLoginException loginException) {
+            System.out.println(loginException.getMessage());
+
+        }catch (WrongPasswordException passwordException) {
+            System.out.println(passwordException.getMessage());
         }catch (Exception e){
-            e.getMessage();
+            System.out.println(e.getMessage());
         }
-        System.out.println("The Login contain _ character: " + isLoginContainUnderscore+
+       /* System.out.println("The Login contain _ character: " + isLoginContainUnderscore+
                 "\nand contain 20 or more 20 characters: " + isLoginContainMoreThan20Char+
                 "\nPassword contain ! characters:" + isPasswordContainExclamationPoint +
                 "\nand contain 20 or more than 20 characters " + isPasswordContainMoreThan20Char+
-                "\nand password identical to confirmPassword: " + isPasswordSameToConfPassw);
+                "\nand password identical to confirmPassword: " + isPasswordSameToConfPassw);*/
+    }
+
+
+
+    public static boolean isLoginContainUnderscore(String login) throws WrongLoginException{
+    if(login.contains("_") == false) {
+            throw new WrongLoginException("Incorrect input - login doesn't contain _ sign");
+    }else
+    return login.contains("_");
+    }
+    public static boolean isLoginContainMoreThan20Char(String login) throws WrongLoginException {
+        if((login.length() >= 20) == true) {
+            throw new WrongLoginException("The Login doesn't contain 20 or more than 20 characters");
+        }else
+        return login.length() >= 20;
+    }
+    public static boolean isPasswordContainExclamationPoint(String password) throws WrongPasswordException{
+        if(password.contains("!") == false) {
+            throw new WrongPasswordException("The Password doesn't contain ! sign");
+        }else
+        return password.contains("!");
+    }
+    public static boolean isPasswordContainMoreThan20Char(String password) throws WrongPasswordException{
+        if((password.length() >= 20) == true) {
+            throw new WrongPasswordException("The Login doesn't contain 20 or more than 20 characters");
+        }else
+        return password.length()>=20;
+    }
+    public static boolean isPasswordSameToConfPassw(String password, String confirmPassword) throws WrongPasswordException{
+        if((password == confirmPassword) == false) {
+            throw new WrongPasswordException("The Login doesn't contain 20 or more than 20 characters");
+        }else
+        return (password == confirmPassword);
     }
 
 
