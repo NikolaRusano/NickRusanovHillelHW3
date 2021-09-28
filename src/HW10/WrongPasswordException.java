5807e0bfd4f818e3a4400c04c5683982ad0f427d
NikolaRusano <36786249+NikolaRusano@users.noreply.github.com>
2021-09-28 23:14:23 +0300

    HW10_update_Pass_exc

--- src/HW10/WrongPasswordException.java
+++ src/HW10/WrongPasswordException.java
@@ -1,8 +1,6 @@
 package HW10;
 
 public class WrongPasswordException extends Exception{
-    public WrongPasswordException() {
-    }
 
     public WrongPasswordException(String message) {
         super(message);
